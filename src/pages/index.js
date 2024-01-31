37f432026e8bfb8cc411d99cf2a83350e94d8c8e
Uiyoung Kim <s221683@student.dhbw-mannheim.de>
2024-01-31 20:23:17 +0100

    making endpoint and connecting to the client side

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import TypingAnimation from "../components/TypingAnimation";
 
 export default function Home() {
   const [inputValue, setInputValue] = useState("");
@@ -17,12 +18,17 @@ export default function Home() {
     setInputValue("");
   };
 
+  // Client Side
   const sendMessage = (message) => {
-    const url = "https://api.openai.com/v1/chat/completions";
+    const url = "/api/chat";
+
+    /*
     const Authentification = `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`;
     const headers = {
+      "Content-type": "application/json",
       Authorization: Authentification,
     };
+    */
 
     const data = {
       model: "gpt-4-turbo-preview",
@@ -33,7 +39,7 @@ export default function Home() {
     setIsLoading(true);
 
     axios
-      .post(url, data, { headers: headers })
+      .post(url, data)
       .then((response) => {
         console.log(response);
         setChatLog((prevChatLog) => [
@@ -49,20 +55,61 @@ export default function Home() {
   };
 
   return (
-    <>
-      <h1>ChatGPT</h1>
-      {chatLog.map((message, index) => (
-        <div key={index}> {message.message} </div>
-      ))}
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Type a message"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <button type="submit">Send</button>
-      </form>
-    </>
+    <div className="container mx-auto max-w-[700px]">
+      <div className="flex flex-col h-screen bg-gray-900">
+        <h1 className="bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text text-center py-3 font-bold text-6xl">
+          ChatGPT
+        </h1>
+
+        <div className="flex-grow p-6">
+          <div className="flex flex-col space-y-4">
+            {chatLog.map((message, index) => (
+              <div
+                key={index}
+                className={`flex ${
+                  message.type === "user" ? "justify-end" : "justify-start"
+                }`}
+              >
+                <div
+                  className={`${
+                    message.type === "user" ? "bg-purple-500" : "bg-gray-800"
+                  } rounded-lg p-4 text-white max-w-sm`}
+                >
+                  {message.message}
+                </div>
+              </div>
+            ))}
+
+            {isLoading && (
+              <div key={chatLog.length} className="flex justify-start">
+                <div className="bg-gray-800 rounded-lg p-4 text-white max-w-sm">
+                  <TypingAnimation />
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+
+        <form onSubmit={handleSubmit} className="flex-none p-6">
+          <div className="flex rounded-lg border border-gray-700 bg-gray-800">
+            <input
+              type="text"
+              className="flex-grow px-4 py-2 bg-transparent text-white focus:outline-none"
+              placeholder="Type a message"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+            <div className="text-right">
+              <button
+                type="submit"
+                className="bg-purple-500 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-purple-600 transition-colors duration-300"
+              >
+                Send
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
