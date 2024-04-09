02ef97d7ecc661363ff43298889375a383da2d58
Uiyoung Kim <s221683@student.dhbw-mannheim.de>
2024-04-09 19:17:17 +0200

    email endpoint works

--- src/pages/api/email/emailSetup.js
+++ src/pages/api/email/emailSetup.js
@@ -1,7 +1,9 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.GOOGLE_EMAIL,
     pass: process.env.GOOGLE_PASSWORD,
