3e05500be1b47b0a0635a117ef4f9b6dfed35b7d
Uiyoung Kim <s221683@student.dhbw-mannheim.de>
2024-04-15 16:35:04 +0200

    new logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,30 +2,34 @@ import Authentication from "./Authentication";
 import Contact from "./Contact";
 
 const handleLogout = () => {
-  fetch('http://localhost:3000/api/user', {
-    method: 'POST',
+  fetch("http://localhost:3000/api/user", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      action: 'logout'
-  }),
+      action: "logout",
+    }),
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log('Success:', data);
+      console.log("Success:", data);
     })
     .catch((error) => {
-      console.error('Error:', error);
+      console.error("Error:", error);
     });
-}
+};
 
 function Navbar() {
   return (
     <nav className="bg-gray-800 shadow-lg w-full fixed top-0 z-50">
       <div className="w-full px-2 h-16 flex">
         <div className="w-full space-x-5 flex items-center justify-center">
-          <img className="h-12 w-auto" src="/logo.innoAssist.png" alt="Logo" />
+          <img
+            className="h-12 w-auto"
+            src="/innoassist_logo_transparent.png"
+            alt="Logo"
+          />
 
           <a
             href="#"
